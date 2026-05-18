5ea71b5f2e718e9a38426e2e68eaf55d8507c5df
coool8192-arch <coool8192@gmail.com>
2026-05-18 21:43:57 +0900

    260518 수출용 패키지

---
+++ Program/Assets/Class3rd (User Interface)/Scripts/Main Menu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Start");
+    }
+
+    public void ShowSetting()
+    {
+        Debug.Log("Setting");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+    }
+}
